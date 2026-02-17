8a7073cd8b9211d2a2785fd611f24e7e93138166
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-17 20:06:39 +0530

    Update keypair.tf

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "mykp" {
-  key_name = "terraform-keypair"
-public_key = file("/root/.ssh/id_rsa.pub")
+  key_name   = "terraform-keypair"
+  public_key = file("${path.module}/terraform-key.pub")
 }
