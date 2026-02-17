304ec01d78f04ccc73a2836c319f098c8cc49693
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-17 20:08:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,51 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "devops_app" {
-  ami           = "ami-019715e0d74f695be"
-  instance_type = "t3.small"
+##################################################
+# Security Group
+##################################################
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH and App traffic"
 
-  # ✅ Attach keypair from keypair.tf
-  key_name = aws_key_pair.mykp.key_name
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##################################################
+# EC2 Instance
+##################################################
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mykp.key_name
+
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
   tags = {
-    Name = "devops-assignment-instance"
+    Name = "DevOps-EC2"
   }
+}
 
   user_data = <<-EOF
               #!/bin/bash
