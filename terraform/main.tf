5efaacb5e5ce4a80f6bbc777c78b58b131cec9c6
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-18 12:07:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-#############################
-# Latest Amazon Linux AMI
-#############################
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -15,44 +12,10 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-#############################
-# Security Group
-#############################
 resource "aws_security_group" "devops_sg" {
-  name = "devops-ec2-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ...
 }
 
-#############################
-# EC2 Instance
-#############################
 resource "aws_instance" "devops_ec2" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
@@ -66,4 +29,3 @@ resource "aws_instance" "devops_ec2" {
     Name = "devops-github-actions-ec2"
   }
 }
-
