1644485d0b7f252d506a581c7ba31c3dd96c155a
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-19 10:59:14 +0530

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+set -e
 
-sudo yum update -y
+# Update system
+dnf update -y
 
-# Install Docker
-sudo yum install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
+# Install required packages
+dnf install -y git docker
+
+# Enable & start Docker
+systemctl enable docker
+systemctl start docker
+
+# Install Docker Compose v2
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 # Add ec2-user to docker group
-sudo usermod -aG docker ec2-user
-
-# Install Git
-sudo yum install git -y
-
-# Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
+usermod -aG docker ec2-user
